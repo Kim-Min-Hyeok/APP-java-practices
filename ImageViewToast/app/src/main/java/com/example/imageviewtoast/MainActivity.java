6d02c49cf8b58accb7fc27d96d48b8b5e185bdbf
Kim-Min-Hyeok <21900102@handong.edu>
2022-01-25 17:13:47 +0900

    ImageView&Toast

---
+++ ImageViewToast/app/src/main/java/com/example/imageviewtoast/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.imageviewtoast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView test;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    test = (ImageView) findViewById(R.id.test);
+    test.setOnClickListener(new View.OnClickListener() {
+
+        @Override
+        public void onClick(View view) {
+            Toast.makeText(getApplicationContext(),"성공입니다.",Toast.LENGTH_SHORT).show();
+        }
+    });
+    }
+}
