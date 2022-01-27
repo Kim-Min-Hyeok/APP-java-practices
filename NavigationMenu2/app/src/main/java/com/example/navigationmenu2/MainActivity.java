796c0b42cccb56a1384ea4e4002be8c5a4118275
Kim-Min-Hyeok <21900102@handong.edu>
2022-01-27 13:54:29 +0900

    NavigationMenu2

---
+++ NavigationMenu2/app/src/main/java/com/example/navigationmenu2/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.navigationmenu2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private View drawerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+    }
+}
